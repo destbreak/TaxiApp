02d3f19e80d286a85a04af0d1442c9806e81f3bc
Reynaldi Hadiansyah <rey.hadiansyah7th@gmail.com>
2020-03-22 13:17:00 +0700

    Add - Some Variable in 'Program.cs' & TaxiInfo Structure in 'Taxi.cs'

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Collection.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +10,13 @@ namespace TaxiApp
     {
         static void Main(string[] args)
         {
-            
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
         }
     }
 }
--- Taxi.cs
+++ Taxi.cs
@@ -1,13 +1,31 @@
 using System;
-using System.Collection.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Taxi
+namespace TaxiApp
 {
     public class Taxi
     {
-        
+        public string DriverName { get; set; }
+        public string DriverStatus { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name          : {0}", DriverName);
+            if(OnDuty == true)
+            {
+              DriverStatus = "Yes";
+              Console.WriteLine("On Duty              : {0}", DriverStatus);
+              Console.WriteLine("Number of Passenger  : {0}", NumPassenger);
+            }else
+            {
+              DriverStatus = "No";
+              Console.WriteLine("On Duty              : {0}", DriverStatus);
+            }
+        }
     }
 }
